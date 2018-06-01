490b4def6778383fa0900f5eb53a69b0459a4b51
Sadeq Rahman <sadeq@MacBook-Pro-Sadeq-Rahman.local>
2018-06-01 07:41:43 -0500

    updated connection.js with heroku info

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,30 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+  "development": {
+    "username": "xawecbfl9w3rdkq2",
+    "password": "b2994owxvzpuip5r",
+    "database": "p4ns1d1yui4aama5",
+    "host": "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "root",
+    "database": "burgers_db",
+    "host": "localhost",
+    "port": 3306,
+    "dialect": "mysql",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
-  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
